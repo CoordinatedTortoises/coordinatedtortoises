781e8b1aeaff2f776e8c1abf9c930736a202bee4
JulianKnodt <julianknodt@gmail.com>
2016-09-01 15:35:29 -0700

    Removed comments on spec

--- specs/dbSpecs.js
+++ specs/dbSpecs.js
@@ -4,12 +4,12 @@ var m = require('mocha');
 var chai = require('chai');
 var assert = require('assert');
 
-// describe('Database', function(){
-//   it('should not be undefined', function(){
-//     assert.equal(Boolean(db.db), true);
-//   });
-//   it('should have a preference table', function(){
-//     assert.equal(Boolean(db.db.models.preferences) && Boolean(db.preferences), true);
-//   })
-// });
+describe('User Preferences Database', function(){
+  it('should not be undefined', function(){
+    assert.equal(Boolean(db.db), true);
+  });
+  it('should have a preference table', function(){
+    assert.equal(Boolean(db.db.models.preferences) && Boolean(db.preferences), true);
+  })
+});
 
