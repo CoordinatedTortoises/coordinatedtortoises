a09f952740ac8c1f0a711b606604bc93ba000ef8
JulianKnodt <julianknodt@gmail.com>
2016-09-01 11:15:55 -0700

    Changed ORM style to listen for connect event

--- db/prefData/userPrefDB.js
+++ db/prefData/userPrefDB.js
@@ -2,30 +2,22 @@ var orm = require('orm');
 
 // https://github.com/dresende/node-orm2
 // https://launchschool.com/blog/how-to-install-postgresql-on-a-mac
+var database = 'userPrefs';
 
-var config = {
-  user: '', //env var: PGUSER
-  database: 'userPrefs', //env var: PGDATABASE
-  password: '', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
-  port: 5432, //env var: PGPORT
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 2000, // how long a client is allowed to remain idle before being closed
+var opts = {
+  user: 'def',
+  database: database,
+  protocol: 'postgres',
+  query:    {pool: true}
 };
-orm.connect("postgres://def:default@localhost/userPrefs", function(err, db){
-  console.log(err);
-  console.log(db);
+
+var db = orm.connect(opts);
+
+db.on('connect', function(err){
   var preference = db.define("preference", {
     id: { type: 'serial', key: true },
     preference: Buffer,
     accountInfo: Object
-  });
-  db.sync(function(err){
-    preference.create({ id: 1}, function(err){
-      preference.exists({id:1}, function(err, prefs){
-        console.log(prefs);
-      })
-    });
-  });
+  }).sync();
 });
 
