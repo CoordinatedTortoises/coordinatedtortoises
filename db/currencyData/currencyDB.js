036c52ec55cea23df377c50614eb77dd4c88c6ac
JulianKnodt <julianknodt@gmail.com>
2016-09-01 16:20:02 -0700

    Added basic rethinkDB with a currencyDB and bitcoin table

--- db/currencyData/currencyDB.js
+++ db/currencyData/currencyDB.js
@@ -1,1 +1,25 @@
 //creating the currency database
+
+//Install with brew install rethinkdb
+//start with brew services start rethinkdb
+
+var rdash = require('rethinkdbdash')({
+  db: 'currencyData',
+  pool: true,
+  cursor: false,
+  timeoutGb: 100
+});
+
+
+rdash.dbList().contains('currencyData')
+  .do(function(exists) {
+  return rdash.branch(exists, { dbs_created: 0 }, rdash.dbCreate('currencyData'));
+    //Rdash branch checks to see whether the first arg is true, and it returns arg 2 if false, or third arg if it isn't.
+}).run();
+rdash.tableList().contains('bitcoinData')
+  .do(function(exists){
+    console.log(exists);
+    return rdash.branch(exists, {tables_created: 0}, rdash.tableCreate('bitcoinData', {
+      durability: 'soft'
+    }));
+  }).run(console.log);
