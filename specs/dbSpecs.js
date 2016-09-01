5e8edfb9da8a7036b2aa2d2294309c0f08f1f5ea
JulianKnodt <julianknodt@gmail.com>
2016-09-01 12:27:47 -0700

    Added a realtest npm script that runs all of the test suites
    and added a basic skeleton for adding yo own test

--- specs/dbSpecs.js
+++ specs/dbSpecs.js
@@ -1,1 +1,11 @@
-var db = require('./db/prefData/userPrefDB');
+var db = require('../db/prefData/userPrefDB'); 
+
+var m = require('mocha');
+var chai = require('chai');
+var assert = require('assert');
+
+describe('test', function(){
+  it('should run this test', function(){
+    assert.equal(true, true);
+  });
+});
