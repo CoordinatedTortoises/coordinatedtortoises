be3d1e25087f66272aedaa9d8608cbe11d49495b
JulianKnodt <julianknodt@gmail.com>
2016-09-01 22:27:58 -0700

    Commented tests back in

--- specs/dbSpecs.js
+++ specs/dbSpecs.js
@@ -3,16 +3,11 @@ var db = require('../db/prefData/userPrefDB');
 var chai = require('chai');
 var assert = require('assert');
 
-// describe('User Preferences Database', function(){
-//   it('should not be undefined', function(){
-//     assert.equal(Boolean(db.db), true);
-//   });
-//   it('should have a preference table', function(){
-//     assert.equal(Boolean(db.db.models.preferences) && Boolean(db.preferences), true);
-//   })
-// });
-
-db.findAll(db.preferences, function(err, res){
-  console.log(err, res);
-  console.log(JSON.stringify(res));
-})
+describe('User Preferences Database', function(){
+  it('should not be undefined', function(){
+    assert.equal(Boolean(db.db), true);
+  });
+  it('should have a preference table', function(){
+    assert.equal(Boolean(db.db.models.preferences) && Boolean(db.preferences), true);
+  })
+});
