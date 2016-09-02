07af2a1f9ae6e934ebd0d2b7621ee52b6f74d155
oldchevy <nick.olszowy@gmail.com>
2016-09-01 20:18:49 -0700

    Init React app homepage structure

---
+++ Public/app/Components/App.jsx
@@ -0,0 +1,57 @@
+class App extends React.component {
+  constructor(props) {
+
+    //Short hand for calling React.component.call(props)
+    super(props);
+
+
+    this.state = {
+      //Put states that change in here
+
+    };
+  }
+
+  //Define class methods here. A couple ones used in the sprint were rendering and lifecycle events
+  //render is necessary to display the JSX supplied to the DOM!
+  //componenetDidMount is called once for every
+
+  logout() {}
+
+  savePrefs() {}
+
+  serverReqs() {}
+
+
+
+  render() {
+    return (
+      //Write JSX here, and passing down important props, like top level methods
+      //Components:
+        //Side bar nav
+          //Save Prefs
+          //LogOut
+        //Main view
+          //Graph 1
+          //Settings for it
+
+          //Props are given as attributes
+      <div>
+        <div class="col-md-4">    
+          <NavBar logout={this.logout} savePrefs={this.savePrefs} />
+        </div>
+        <div class="col-md-8">    
+          <Main logout={this.logout} savePrefs={this.savePrefs} />
+        </div>
+      </div>
+
+
+
+    );
+  }
+
+
+}
+
+
+//ES6 makes you expose things to the window, similar to in Node
+window.App = App;
