87453dc5a190f06aee9579475a29c87fccfe018c
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-09-01 17:24:04 -0700

    set up server

--- server/index.js
+++ server/index.js
@@ -16,6 +16,18 @@ app.post('/', function(req, res) {
   res.send('testing post.');
 });
 
+//find all users, send them back
+app.get('/users', function(req, res) {
+  //get all users from db
+    //send back all users
+  res.status(200).send(JSON.stringify(users));
+});
+
+//new user submitted, add new user to db
+app.post('/users', function(req, res) {
+  console.log(req);
+});
+
 app.listen(3000, function() {
   console.log('server listening at port 3000');
 });
