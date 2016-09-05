8aa9803f1a1cf9b97ec827c0d00ccd7067289df9
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-09-05 15:41:34 -0700

    Add handlers

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var bp = require('body-parser');
 var mo = require('method-override');
 var db = require('../db/prefData/userPrefDB.js').db;
+var session = require('express-session');
 
 //---------- database methods ----------------//
 //preferences: id, preference, accountInfo
@@ -12,48 +13,57 @@ var db = require('../db/prefData/userPrefDB.js').db;
 
 //-------- server set up ----------//
 var app = express();
-
+//initialize session
+app.use(session({secret: 'secret'}));
 app.use(express.static(__dirname + '/public'));
 
 
-//------- root handling ---------//
+//-------------------------- ROOT -------------------------//
 app.get('/', function(req, res) {
-  res.status(200).send();
-});
-
-app.post('/', function(req, res) {
-  res.send('testing post.');
+  console.log('BEFORE LOG IN: ');
+  if (req.session.username && req.session.password) {
+    console.log(req.session.username + ' has been logged in.');
+    res.redirect('/users/preferences');
+  }
 });
 
 
 //----------- main page -------------//
 
 //find all users, send them back
-app.get('/', function(req, res) {
+app.get('/users/preferences', function(req, res) {
   //get all users from db
-  findAll(prefs, function(err, userPrefs) {
-    if (err) {
-      console.log('Oops! error: ', err);
-    } else {
-      console.log('Got all user prefs: ', userPrefs);
-      res.status(200).send(JSON.stringify(userPrefs));
-    }
-  });
+  if (req.session.username && req.session.password) {
+    db.findAll(db.preferences, function(err, prefs) {
+      if (err) {
+        console.log('Oops! error: ', err);
+      } else {
+        console.log('Got all user prefs: ', prefs);
+        res.json(prefs);
+      }
+    });  
+  } else {
+    alert('Please log in.');
+    res.redirect('/login');
+  }
+});
+
+app.get('/login', function(req, res) {
+  res.render('login.html');
 });
 
 //new user submitted, add new user to db
-app.post('/', function(req, res) {
-  //add(model, options, callback)
-  add(prefs, req.body, function(err, newPref) {
-    //if error
+app.post('/login', function(req, res) {
+  req.session.username = req.body.username;
+  req.session.password = req.body.password;
+
+  db.add(db.users, req.body, function(err, newUser) {
     if (err) {
-      //log error.
-      console.log('error in adding new user pref: ', err);
+      console.log('error in adding new user: ', err);
       return;
     }
-    //no error, log new user and send result
     console.log('New user added: ' + req.body);
-    res.json(newPref);
+    res.json(newUser);
   });
 });
 
