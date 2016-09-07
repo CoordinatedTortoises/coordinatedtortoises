c4519d8b038013ba175bc3a14b1280a8793f0ec3
JulianKnodt <julianknodt@gmail.com>
2016-09-07 01:26:46 -0700

    Commented up paycoin.py and added an export for paycoin.js

--- server/paycoin/paycoin.js
+++ server/paycoin/paycoin.js
@@ -1,17 +1,32 @@
 //Have to install 'python-shell'
 
 var PythonShell = require('python-shell');
-var pyshell = new PythonShell('paycoin.py');
+var pyReq = new PythonShell('paycoin.py');
  
 // sends a message to the Python script via stdin 
-pyshell.send('hello');
+// pyReq.send('hello');
  
-pyshell.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement) 
-  console.log(message);
-});
+// received a message sent from the Python script (a simple "print" statement) 
+// pyReq.on('message', function (message) {
+  // console.log(message);
+// });
  
 // end the input stream and allow the process to exit 
-pyshell.end(function (err) {
-  if (err) throw err;
-});
+// pyReq.end(function (err) {
+//   if (err) throw err;
+// });
+
+
+module.exports = function(serviceUrl, callback){
+  pyReq.send(serviceUrl);
+  pyReq.on('message', function(message){
+    callback(message);
+    pyReq.end(function(err){
+      if (err) {
+        throw err;
+      }
+    });
+
+  });
+
+}
--- server/paycoin/paycoin.py
+++ server/paycoin/paycoin.py
@@ -1,4 +1,27 @@
 import sys
+import requests
+# sudo pip install requests
+# If you don't have pip, brew install pip
 
-for line in sys.stdin:
-    print line
+# Indentation is python's form of a bracket
+# for/while loops and ifs require a :
+# If's don't require ()
+# Exmaple:
+# if a == b:
+#   do stuff
+# elif c == d:
+#   do other stuff
+# else:
+#   default stuff
+
+# in this file we want to make a get request to our own server,
+# which paays us some amount of 'tip' for using our service, and our server will then make a request
+# to a 21 module that costs money, and we send the result back to the user
+
+for data in sys.stdin:
+  r = requests.get('http://www.python.org')
+  # r = requests.post('http://localhost:3000', data)
+  print resp.text
+
+
+  conn.close()
